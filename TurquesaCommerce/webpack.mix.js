b2ac70b2a110a2b4830e49043396f551acd8fcc4
lswolf <lswolf@hotmail.com.br>
2022-06-27 10:54:19 -0300

    webshop ready for customisation

--- TurquesaCommerce/webpack.mix.js
+++ TurquesaCommerce/webpack.mix.js
@@ -11,11 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ]);
-
-if (mix.inProduction()) {
-    mix.version();
-}
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
