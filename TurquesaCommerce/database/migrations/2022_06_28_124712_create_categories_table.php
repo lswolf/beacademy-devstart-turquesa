9c339b33fb477fd1633c95b3c71f4b579f557c2b
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-06-30 07:55:18 -0300

    feature/migrate-category Alteração

--- TurquesaCommerce/database/migrations/2022_06_28_124712_create_categories_table.php
+++ TurquesaCommerce/database/migrations/2022_06_28_124712_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('categories', 100);
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
