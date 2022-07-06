da144d7d865e24cdfeb9edf22d36ca9cb9330b13
Pablo Henrique <pabloenosteteucpx@gnail.com>
2022-07-06 10:57:50 -0300

    feature/crud-category conluido

--- TurquesaCommerce/routes/web.php
+++ TurquesaCommerce/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('categoria')->group(function () {
+    Route::match(['get', 'post'], '', [CategoryController::class, 'index'])->name('category.index');
+    Route::match(['get', 'post'], '/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::match(['get', 'post'], '/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::match(['get', 'post'], '/edit/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put('/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+});
+
+
 
 Route::middleware([
     'auth:sanctum',
