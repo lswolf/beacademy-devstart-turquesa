c36d83912ccd0f6c5fcb5a076694eea7bce3bb91
amcarv1 <erickamorimcarv@gmail.com>
2022-07-06 23:00:45 -0300

    crud of users created

---
+++ TurquesaCommerce/resources/views/users/create.blade.php
@@ -0,0 +1,19 @@
+<h1>Criar Usuário</h1>
+
+<form action="{{ route('users.store') }}" method="POST">
+    @csrf
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" name="name">
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">E-mail</label>
+      <input type="email" class="form-control" id="email" name="email">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Senha</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </form>
--- TurquesaCommerce/routes/web.php
+++ TurquesaCommerce/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,10 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/user', [UserController::class, 'store'])->name('users.store');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::put('/users/edit/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
