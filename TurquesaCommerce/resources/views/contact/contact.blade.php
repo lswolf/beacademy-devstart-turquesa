ec34a1e4a7ea23d0186fca14374e709ee186ea39
amcarv1 <erickamorimcarv@gmail.com>
2022-07-14 21:30:12 -0300

    forms labels and inputs added

--- TurquesaCommerce/resources/views/contact/contact.blade.php
+++ TurquesaCommerce/resources/views/contact/contact.blade.php
@@ -1,1 +1,36 @@
-<h1>Contato</h1>
+@extends('layouts.main')
+@section('title', 'Editar Categoria')
+@section('content')
+
+<div>
+    <div>
+        <h3>INFORMAÇÕES DE CONTATO</h3>
+
+        <form action="">
+            <div class="mb-3">
+                <label for="name" class="form-label"></label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Nome*">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label"></label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email*">
+              </div>
+              <div class="mb-3">
+                  <label for="phone" class="form-label"></label>
+                  <input type="phone" class="form-control" id="phone" name="phone" placeholder="Telefone*">
+                </div>
+                <div class="mb-3">
+                    <label for="comments" class="form-label"></label>
+                    <input type="text" class="form-control" id="comments" name="comments" placeholder="Comentários*">
+                </div>
+
+                
+              <button type="submit" class="btn btn-primary">Enviar</button>            
+        </form>
+    </div>
+    <div>
+
+    </div>
+</div>
+
+@endsection
