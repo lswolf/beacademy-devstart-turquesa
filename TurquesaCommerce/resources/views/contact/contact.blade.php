c1e7afe7a2bf3811d851e2d2ecd134897227789d
amcarv1 <erickamorimcarv@gmail.com>
2022-07-14 21:36:17 -0300

    talk with us section added html structure

--- TurquesaCommerce/resources/views/contact/contact.blade.php
+++ TurquesaCommerce/resources/views/contact/contact.blade.php
@@ -3,10 +3,49 @@
 @section('content')
 
 <div>
+
+    <div>
+        <h2>FALE CONOSCO</h2>
+
+        <div>
+            <h3>TELEFONE</h3>
+            <div>
+                <img src="" alt="">
+                <p>(xx) xxxx-xxxx</p>
+                <p>Whats App:(xx) xxxx-xxxx</p>
+            </div>
+        </div>
+
+        <div>
+            <h3>EMAIL</h3>
+            <div>
+                <img src="" alt="">
+                <p>email@email.com</p>
+            </div>
+        </div>
+
+        <div>
+            <h3>Redes Sociais</h3>
+            <div>
+                <img src="" alt="">
+                <p>email@email.com</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p>email2@email.com</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p>email3@email.com</p>
+            </div>
+        </div>
+    </div>
+
     <div>
         <h3>INFORMAÇÕES DE CONTATO</h3>
 
-        <form action="">
+        <form action="" method="POST">
+            @csrf
             <div class="mb-3">
                 <label for="name" class="form-label"></label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Nome*">
@@ -23,13 +62,8 @@
                     <label for="comments" class="form-label"></label>
                     <input type="text" class="form-control" id="comments" name="comments" placeholder="Comentários*">
                 </div>
-
-                
               <button type="submit" class="btn btn-primary">Enviar</button>            
         </form>
-    </div>
-    <div>
-
     </div>
 </div>
 
