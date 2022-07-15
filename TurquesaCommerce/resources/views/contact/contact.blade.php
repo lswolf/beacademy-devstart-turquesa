c5bc7a3c64fa16fe8cdc829d013e9322304e4dde
amcarv1 <erickamorimcarv@gmail.com>
2022-07-14 22:25:42 -0300

    contact view front-end completed

--- TurquesaCommerce/resources/views/contact/contact.blade.php
+++ TurquesaCommerce/resources/views/contact/contact.blade.php
@@ -2,47 +2,49 @@
 @section('title', 'Editar Categoria')
 @section('content')
 
-<div>
+<div class="d-flex" style="margin-left: 50px; margin-bottom: 50px">
 
-    <div>
+    <div style="width: 500px; margin-top: 50px">
         <h2>FALE CONOSCO</h2>
 
-        <div>
-            <h3>TELEFONE</h3>
-            <div>
-                <img src="" alt="">
-                <p>(xx) xxxx-xxxx</p>
-                <p>Whats App:(xx) xxxx-xxxx</p>
+        <div style="margin-top: 50px">
+            <h3 style="font-size: 20px">TELEFONE</h3>
+            <div class="d-flex">
+                <img src="/img/phone.svg" alt="">
+                <div style="margin-left: 5px">
+                    <p style="margin: 0; margin-top: 15px; font-weight: 500;">(31) 3557-3333</p>
+                    <p style="font-weight: 500;">WhatsApp:(91) 99999-1990</p>
+                </div>
             </div>
         </div>
 
         <div>
-            <h3>EMAIL</h3>
-            <div>
-                <img src="" alt="">
-                <p>email@email.com</p>
+            <h3 style="font-size: 20px; margin-bottom: 10px">EMAIL</h3>
+            <div class="d-flex">
+                <img src="/img/email.svg" alt="">
+                    <p style="margin-left: 5px; padding-top: 20px; font-weight: 500;">comercial@turquesabooks.com</p>
             </div>
         </div>
 
         <div>
-            <h3>Redes Sociais</h3>
-            <div>
-                <img src="" alt="">
-                <p>email@email.com</p>
+            <h3 style="font-size: 20px; margin-top: 10px">REDES SOCIAIS</h3>
+            <div class="d-flex">
+                <img src="/img/instagram.svg" alt="">
+                    <p style="margin-left: 5px; padding-top: 20px; font-weight: 500;">comercial@turquesabooks.com</p>
             </div>
-            <div>
-                <img src="" alt="">
-                <p>email2@email.com</p>
+            <div class="d-flex">
+                <img src="/img/facebook.svg" alt="">
+                    <p style="margin-left: 5px; padding-top: 20px; font-weight: 500;">comercial@turquesabooks.com</p>
             </div>
-            <div>
-                <img src="" alt="">
-                <p>email3@email.com</p>
+            <div class="d-flex">
+                <img src="/img/twitter.svg" alt="">
+                    <p style="margin-left: 5px; padding-top: 20px; font-weight: 500;">comercial@turquesabooks.com</p>
             </div>
         </div>
     </div>
 
-    <div>
-        <h3>INFORMAÇÕES DE CONTATO</h3>
+    <div style="margin-left: 100px; margin-top: 80px">
+        <h3 style="font-size: 20px">INFORMAÇÕES DE CONTATO</h3>
 
         <form action="" method="POST">
             @csrf
@@ -59,12 +61,12 @@
                   <input type="phone" class="form-control" id="phone" name="phone" placeholder="Telefone*">
                 </div>
                 <div class="mb-3">
-                    <label for="comments" class="form-label"></label>
-                    <input type="text" class="form-control" id="comments" name="comments" placeholder="Comentários*">
+                    <textarea name="" id="" cols="80" rows="10"></textarea>
                 </div>
               <button type="submit" class="btn btn-primary">Enviar</button>            
         </form>
     </div>
+
 </div>
 
 @endsection
