1ce69a31b9819a2c70bbff11d7adab8792d23d65
= <bethaniafreitas@yahoo.com.br>
2022-07-21 20:25:50 -0300

    edição do footer

--- TurquesaCommerce/resources/views/layouts/main.blade.php
+++ TurquesaCommerce/resources/views/layouts/main.blade.php
@@ -100,12 +100,85 @@
 
 
   @yield('content')
-  <footer class="d-flex align-items-center justify-content-center ">
-    <p>Copyright &copy 2022 Todos os direitos reservados</p>
-  </footer>
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
+  <footer>
+  <nav class="navbar navbar-dark bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h5 class="white-text">Sobre nós</h5>
+          <p class="grey-text text-lighten-4">Este projeto foi desenvolvido por alunos do programa DevStart da Be.Academy em parceria com a Paylivre. 
+              O objetivo era criar uma plataforma online com Crud, Checkout e Autenticação, feito em PHP pelo framework Laravel. 
+          </p>
+        </div>  
+        <div class="col-3">
+        <ul>
+          <h5 class="white-text">Alunos:</h5>
+
+          <a class="navbar-brand" href="https://github.com/arthur-fellipe" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/arthur-cerqueira/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Arthur Fellipe    
+          </a>  
+
+          <a class="navbar-brand" href="https://github.com/Bethania-Freitas" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/bethania-alves-de-oliveira-e-freitas-90538b15a/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Bethânia Freitas     
+          </a>     
+
+          <a class="navbar-brand" href="https://github.com/DanielPinheir" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/daniel-pinheiro-machado-9874329a/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Daniel Pinheiro    
+          </a>  
+
+          <a class="navbar-brand" href="https://github.com/amcarv1" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/erick-amorim-265667214/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Erick Amorin    
+          </a>  
+
+          
+        </div>
+        <div class="col-3"><br>
+
+          <a class="navbar-brand" href="https://github.com/eedvansilva" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/edvansilvaa/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Edvan Silva    
+          </a>  
+
+          <a class="navbar-brand" href="https://github.com/GeanFerreira" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/ferreiragean/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Gean Ferreira  
+          </a> 
+
+          <a class="navbar-brand" href="https://github.com/lswolf" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/lucas-rodrigues-87763bb7/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Lucas Rodrigues 
+          </a> 
+
+          <a class="navbar-brand" href="https://github.com/Pablohenrique2" > 
+              <img height="24" src="/img/icons8-github.svg" alt="Link do Github">   
+          <a class="navbar-brand" href="https://www.linkedin.com/in/pablo-henrique-832445203/" >     
+              <img height="24" src="/img/icons8-linkedin-circundado.svg" alt="Link do Github">   -  Pablo Henrique
+          </a> 
+        
+        </div>  
+      </div>
+    </div>
+  </div>
+</nav>
+      <nav class="navbar navbar-dark bg-info"> 
+        <div class="container">
+          <p class="text-center">Grupo Turquesa 🤍</p>
+        </div>
+        <br>
+      </nav>
+  </footer>
 </body>
 
 </html>
