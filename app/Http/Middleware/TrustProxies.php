3ba6f2b7960664acbed74ced4f12b7e9699923f5
lswolf <lswolf@hotmail.com.br>
2022-08-03 19:02:14 -0300

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
+//use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-
+use Fideloper\Proxy\TrustProxies as Middleware;
 class TrustProxies extends Middleware
 {
     /**
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
@@ -20,9 +20,9 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
+        //Request::HEADER_X_FORWARDED_FOR |
+        //Request::HEADER_X_FORWARDED_HOST |
+       // Request::HEADER_X_FORWARDED_PORT |
+       // Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
 }
