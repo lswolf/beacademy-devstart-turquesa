52bb2b793120a5a03b2fba1130361a63d45b2c2f
Erick Amorim <erickamorimcarv@gmail.com>
2022-08-04 11:28:20 -0300

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\PayPalPaymentController;
 use App\Http\Controllers\ProductController;
 use App\Models\Payment;
 use Illuminate\Support\Facades\Route;
