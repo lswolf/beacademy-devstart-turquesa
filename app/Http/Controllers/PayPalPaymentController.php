e162f6292648047bc87a5647a7b3b5a130138139
lswolf <lswolf@hotmail.com.br>
2022-08-03 21:43:52 -0300

    added paypal support

---
+++ app/Http/Controllers/PayPalPaymentController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Srmklive\PayPal\Services\ExpressCheckout;
+class PayPalPaymentController extends Controller
+{
+    public function handlePayment(Request $request)
+    {
+        $product = [];
+        $product['items'] = [
+            [
+                'name' => $request->products,
+                'price' => $request->amount,
+                'desc'  => '',
+                'qty' => 1
+            ]
+            ];
+        $product['invoice_id'] = $request->order_id;
+        $product['invoice_description'] = "Ordem #{$product['invoice_id']} pedido";
+        $product['return_url'] = route('success.payment');
+        $product['cancel_url'] = route('cancel.payment');
+        $product['total'] = 224;
+  
+        $paypalModule = new ExpressCheckout;
+  
+        $res = $paypalModule->setExpressCheckout($product);
+        $res = $paypalModule->setExpressCheckout($product, true);
+  
+        return redirect($res['paypal_link']);
+    }
+   
+    public function paymentCancel()
+    {
+        return redirect()->route('viewcart')->with('cartErr', 'Compra Cancelada!');
+    }
+  
+    public function paymentSuccess(Request $request)
+    {
+        $paypalModule = new ExpressCheckout;
+        $response = $paypalModule->getExpressCheckoutDetails($request->token);
+  
+        if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESSWITHWARNING'])) {
+            return redirect()->route('viewcart')->with('cartAdd', 'Compra efetuada com sucesso!');
+        }
+  
+        return redirect()->route('viewcart')->with('cartErr', 'Compra Cancelada!');
+    }
+}
